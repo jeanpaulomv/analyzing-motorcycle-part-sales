e400cce0430068b52c58153ab64f4afe84aafd34
Jean Paulo Moran Vanoni <jeanpaulomv@homtail.com>
2024-10-20 06:15:47 -0500

    Eliminado entorno virtual del repositorio

--- src/import_csv_to_postgresql.py
+++ src/import_csv_to_postgresql.py
@@ -15,6 +15,10 @@ engine = create_engine(db_url)
 # Read CSV file
 df = pd.read_csv('sales.csv')
 
+# Clean null or invalid data
+df.dropna(inplace=True) 
+df['date'] = pd.to_datatime(df['date'], errors='coerce')
+
 # Create the table automatically and import the data
 df.to_sql('sales', engine, index=False, if_exists='replace')
 
